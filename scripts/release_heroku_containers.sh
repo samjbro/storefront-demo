121711fc20211ccda9de6cb892913e6ecdeaf429
Sam Broughton <sjbroughton1@gmail.com>
2019-05-17 03:40:23 -0500

    removes failed attempt to login to heroku programmatically

--- scripts/release_heroku_containers.sh
+++ scripts/release_heroku_containers.sh
@@ -1,7 +1,3 @@
-(
-  echo "$HEROKU_CREDENTIALS_EMAIL"
-  echo "$HEROKU_CREDENTIALS_PASSWORD"
-) | heroku login
 # Release container within Heroku
 heroku container:release web --app turing-samjbro-proxy
 heroku container:release web --app turing-samjbro-client
