96623deb5e5690048190fa98ea5584f8d66829eb
Sam Broughton <sjbroughton1@gmail.com>
2019-05-17 05:50:06 -0500

    trying heroku deployment with new token

--- scripts/release_heroku_containers.sh
+++ scripts/release_heroku_containers.sh
@@ -1,4 +1,4 @@
 # Release container within Heroku
-# heroku container:release web --app turing-samjbro-proxy
-# heroku container:release web --app turing-samjbro-client
-# heroku container:release web --app turing-samjbro-api
+heroku container:release web --app turing-samjbro-proxy
+heroku container:release web --app turing-samjbro-client
+heroku container:release web --app turing-samjbro-api
