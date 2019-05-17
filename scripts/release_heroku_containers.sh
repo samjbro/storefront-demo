d6d508660ae189f076dbdfba1ef9bbf09c472f94
Sam Broughton <sjbroughton1@gmail.com>
2019-05-17 03:33:43 -0500

    fixes proxy dockerfile ref for default.conf and attempts heroku login via cli

--- scripts/release_heroku_containers.sh
+++ scripts/release_heroku_containers.sh
@@ -1,3 +1,7 @@
+(
+  echo "$HEROKU_CREDENTIALS_EMAIL"
+  echo "$HEROKU_CREDENTIALS_PASSWORD"
+) | heroku login
 # Release container within Heroku
 heroku container:release web --app turing-samjbro-proxy
 heroku container:release web --app turing-samjbro-client
