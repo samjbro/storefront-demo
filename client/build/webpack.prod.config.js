cb7795c2842622ce8e87ba13cb8001f59b5fdc08
Sam Broughton <sjbroughton1@gmail.com>
2019-05-17 02:19:43 -0500

    adds prod config file to client

--- client/build/webpack.prod.config.js
+++ client/build/webpack.prod.config.js
@@ -1,8 +1,9 @@
-const path = require('path')
 const merge = require('webpack-merge')
-const base = require('./webpack.base.config')
+const path = require('path')
 
-module.exports = merge(base, {
+const baseConfig = require('./webpack.base.config')
+
+module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
