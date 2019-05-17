64e296b22df6041994745a1a56965c0df55b076c
Sam Broughton <sjbroughton1@gmail.com>
2019-05-17 01:33:31 -0500

    fixes Dockerfile paths

--- scripts/push_to_docker.sh
+++ scripts/push_to_docker.sh
@@ -2,9 +2,9 @@
 
 if [[ "$TRAVIS_BRANCH" = "master" && "$TRAVIS_PULL_REQUEST" = false ]] ; then
 # Build images
-  docker build -t "$DOCKER_ID"/turing-proxy ./proxy
-  docker build -t "$DOCKER_ID"/turing-client ./client
-  docker build -t "$DOCKER_ID"/turing-api ./api
+  docker build -t "$DOCKER_ID"/turing-proxy ./proxy/docker
+  docker build -t "$DOCKER_ID"/turing-client ./client/docker
+  docker build -t "$DOCKER_ID"/turing-api ./api/docker
 # Log in to the Docker CLI
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 # Push images to Docker Hub
