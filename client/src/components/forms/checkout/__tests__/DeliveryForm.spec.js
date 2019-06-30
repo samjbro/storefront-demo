5c606f6b43e7a9bddb529317b1601f1c90898a7e
Sam Broughton <sjbroughton1@gmail.com>
2019-06-29 19:01:37 -0500

    test: DeliveryForm will not attempt to add customer address if input data is incomplete

--- client/src/components/forms/checkout/__tests__/DeliveryForm.spec.js
+++ client/src/components/forms/checkout/__tests__/DeliveryForm.spec.js
@@ -4,7 +4,8 @@ import {
 
 import DeliveryForm from '../DeliveryForm'
 import {
-  SET_SHIPPING_TYPE
+  SET_SHIPPING_TYPE,
+  ADD_CUSTOMER_ADDRESS
 } from '../../../../apollo/operations/mutations';
 
 describe('DeliveryForm', () => {
@@ -22,12 +23,41 @@ describe('DeliveryForm', () => {
       }
     }))
   })
+  it('can set a new customer address', () => {
+    const mutate = jest.fn()
+    const deliveryData = {
+      firstName: 'some-name',
+      lastName: 'some-last-name',
+      address: 'some-address',
+      city: 'some-city',
+      state: 'some-state',
+      zipCode: 'some-zip-code'
+    }
+    const wrapper = renderComponent({
+      mutate,
+      data: {
+        deliveryData
+      }
+    })
+    wrapper.vm.submit()
+    expect(mutate).toHaveBeenCalledWith(expect.objectContaining({
+      mutation: ADD_CUSTOMER_ADDRESS,
+      variables: {
+        data: deliveryData
+      }
+    }))
+  })
+  it('will not attempt to add customer address if input data is incomplete', () => {})
 })
 
 const renderComponent = ({
-  mutate
+  mutate,
+  data
 }) => {
   return mount(DeliveryForm, {
+    data: () => ({
+      ...data
+    }),
     mocks: {
       $apollo: {
         mutate
